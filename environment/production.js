e19ff5b82e86d1cc6077c05b194bd8aae59d324b
Allen Brown <Lenovo@MacBook-Air-2.local>
2025-03-24 20:05:38 -1000

    fix

--- environment/production.js
+++ environment/production.js
@@ -3,6 +3,6 @@ const path = require("path");
 module.exports = {
   dbUrl:
     "mongodb+srv://Allen:C3EjU2rnNUfbPO@cluster0.lg23k.mongodb.net/nodeTest?retryWrites=true&w=majority&appName=Cluster0",
-  cert: path.join(__dirname, ""),
-  key: path.join(__dirname, ""),
+  cert: path.join(__dirname, "../ssl/local.crt"),
+  key: path.join(__dirname, "../ssl/local.key"),
 };
