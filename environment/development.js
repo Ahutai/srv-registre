1e216fd857b07f98feffaf65a2f37fa3209f8a4f
Allen Brown <Lenovo@MacBook-Air-2.local>
2025-03-24 22:13:03 -1000

    update

--- environment/development.js
+++ environment/development.js
@@ -5,4 +5,6 @@ module.exports = {
     "mongodb+srv://Allen:C3EjU2rnNUfbPO@cluster0.lg23k.mongodb.net/nodeTest?retryWrites=true&w=majority&appName=Cluster0",
   cert: path.join(__dirname, "../ssl/local.crt"),
   key: path.join(__dirname, "../ssl/local.key"),
+  portHttp: 3000,
+  portHttps: 3001,
 };
--- environment/production.js
+++ environment/production.js
@@ -3,4 +3,6 @@ module.exports = {
     "mongodb+srv://Allen:C3EjU2rnNUfbPO@cluster0.lg23k.mongodb.net/nodeTest?retryWrites=true&w=majority&appName=Cluster0",
   cert: "/etc/letsencrypt/live/www.brown-allen.com/fullchain.pem",
   key: "/etc/letsencrypt/live/www.brown-allen.com/privkey.pem",
+  portHttp: 80,
+  portHttps: 443,
 };
